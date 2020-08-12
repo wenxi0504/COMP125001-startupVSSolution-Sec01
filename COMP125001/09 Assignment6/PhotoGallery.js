7d81a9409d8cb40607a98a1b1b99cecc0feae1bf
Wen Xu <wxu63@my.centennialcollege.ca>
2020-08-12 13:18:45 -0400

    add JQuery  function  to activate the box photo

--- COMP125001/09 Assignment6/PhotoGallery.js
+++ COMP125001/09 Assignment6/PhotoGallery.js
@@ -1,8 +1,60 @@
-﻿var ourRequest = new XMLHttpRequest()
-ourRequest.open('GET', '../ 09 Assignment6 /PictureLIst.json')
-ourRequest.onload= function() {
+﻿var photoList = document.getElementById("photoItems");
+var mainPhoto = document.getElementById("fimg");
 
-    console.log(ourRequest.responseText);
+var btn = document.getElementById("btn");
+btn.addEventListener("click", pictureList);
+var j=0;
 
-};
-ourRequest.send();
+
+function pictureList() {
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', '../09 Assignment6/PictureLIst.json');
+    ourRequest.onload = function() {
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+    };
+    ourRequest.send();
+}
+
+function renderHTML(data) {
+    var photoListString = "";
+
+    for (i = 1; i < data.length; i++) {
+        photoListString += "<div class=\"box\"><img id=\"photo\" src=\"" + data[i].link +  "\"></div>";
+    }
+
+   
+    photoList.innerHTML = photoListString;
+    mainPhoto.innerHTML = "<img src=\"" + data[1].link + "\">";
+    $('box').refresh;
+
+}
+
+$(document).ready(function () {
+    //$('.box').click(function () {
+    //    $('.full-image').html($(this).html());
+
+    //});
+
+    $('#nextBtn').click(function () {
+        j++;
+        if (j > 3) {
+            j = 0;
+        }
+        $('.full-image').html($('.box').get(j).innerHTML);
+
+    });
+
+    $('#preBtn').click(function () {
+        j--;
+        if (j < 0) {
+            j = 3;
+        }
+        $('.full-image').html($('.box').get(j).innerHTML);
+
+    });
+
+    $(document).on("click", ".box", function() {
+        $('.full-image').html(this.innerHTML);
+    });
+});
